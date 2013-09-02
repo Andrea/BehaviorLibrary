96ed264f4340495ef422def74ae7f83736256134
Andrea <roundcrisis@gmail.com>
2013-09-02 22:37:50 +0100

    more changes to BT example..nearly there

--- BehaviourTreeExample/Program.cs
+++ BehaviourTreeExample/Program.cs
@@ -1,4 +1,6 @@
-﻿using BehaviorLibrary;
+﻿using System;
+
+using BehaviorLibrary;
 using BehaviorLibrary.Components.Actions;
 using BehaviorLibrary.Components.Composites;
 using BehaviorLibrary.Components.Conditionals;
@@ -8,8 +10,11 @@ namespace BehaviourTreeExample
 {
 	public class Program
 	{
+		private static int _behaviour;
+
 		static void Main(string[] args)
 		{
+			
 			var tooClose = new Conditional(isTooClose);
 			var targetMoved = new Conditional(hasTargetMoved);
 			var pathFound = new Conditional(hasPathBeenFound);
@@ -38,16 +43,111 @@ namespace BehaviourTreeExample
 			ParallelSelector ifReachedCellGetNext = new ParallelSelector(new Inverter(reachedCell), getNextCell);
 			ParallelSelector ifNotReachedTargetMoveTowardsCell = new ParallelSelector(reachedTarget, moveToCell);
 
-			ParallelSequence follow = new ParallelSequence(new Inverter(tooClose), updatePosition, ifMovedCreateNewPath, ifPathFoundGetPath, 
-				isNewPath, ifReachedCellGetNext, ifNotReachedTargetMoveTowardsCell, animate);
+			ParallelSequence follow = new ParallelSequence(new Inverter(tooClose), updatePosition, ifMovedCreateNewPath, ifPathFoundGetPath,
+				ifPathNewUseIt, ifReachedCellGetNext, ifNotReachedTargetMoveTowardsCell, animate);
 
-			RootSelector root = new RootSelector(switchBehaviors, initialize, follow);
+			
+			RootSelector root = new RootSelector(SwitchBehaviours, initialize, follow);
 
 			//set a reference to the root
 			var behavior = new Behavior(root);
 
 			//to execute the behavior
 			behavior.Behave();
+			Console.ReadLine();
+		}
+
+		private static BehaviorReturnCode updateAnimation()
+		{
+			return BehaviorReturnCode.Success;
+		}
+
+		private static BehaviorReturnCode resetPathfinder()
+		{
+			return BehaviorReturnCode.Success;
+		}
+
+		private static BehaviorReturnCode updateTargetPosision()
+		{
+			return BehaviorReturnCode.Success;
+		}
+
+		private static BehaviorReturnCode getCurrentPath()
+		{
+			return BehaviorReturnCode.Success;
+		}
+
+		private static BehaviorReturnCode setNewPath()
+		{
+			return BehaviorReturnCode.Success;
+		}
+
+		private static BehaviorReturnCode getNextPathCell()
+		{
+			return BehaviorReturnCode.Success;
+		}
+
+		private static bool hasNewPath()
+		{
+			Console.WriteLine("Has new path");
+			return true;
+		}
+
+		private static bool hasReachedTarget()
+		{
+			Console.WriteLine("Has reached target");
+			return true;
+		}
+
+		private static bool hasReachedCell()
+		{
+			Console.WriteLine("Has reached Cell");
+			return true;
+		}
+
+		private static bool hasPathBeenFound()
+		{
+			Console.WriteLine("Has path been found");
+			return true;
+		}
+
+		private static BehaviorReturnCode initializePathfinder()
+		{
+			Console.WriteLine("init path finder");
+			return BehaviorReturnCode.Success;
+
+		}
+
+		private static BehaviorReturnCode calculatePath()
+		{
+			Console.WriteLine("CAlculate Path");
+			return BehaviorReturnCode.Success;
+		}
+
+		private static BehaviorReturnCode moveTowardsCell()
+		{
+			Console.WriteLine("Move towards cell");
+			return BehaviorReturnCode.Success;
+		}
+
+		private static bool hasTargetMoved()
+		{
+			Console.WriteLine("Has target moved");
+			return true;
+		}
+
+		private static bool isTooClose()
+		{
+			Console.WriteLine("Is too close");
+			return false;
+
+		}
+
+		private static int SwitchBehaviours()
+		{
+			Console.WriteLine("Switch behaviours {0}", _behaviour++);
+			return _behaviour;
+
 		}
 	}
 }
