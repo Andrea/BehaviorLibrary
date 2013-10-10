f66a1759e9ebe92672937ba21551943652e73df8
Michael O'Kane <ml_o_kane@hotmail.com>
2013-10-10 14:28:48 +0100

    Created interuptable behaviour component that can be interupted when a condition is true

---
+++ BehaviorLibrary/Components/Decorators/Interruptable.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using BehaviourLibrary.Components.Conditionals;
+
+namespace BehaviourLibrary.Components.Decorators
+{
+	public class Interruptable : BehaviourComponent
+	{
+		private readonly BehaviourComponent _behaviourComponent;
+		private readonly Conditional _interruptCondition;
+		private readonly BehaviourReturnCode _onInterruptReturn;
+
+		public Interruptable(BehaviourComponent behaviour, Conditional interruptCondition,
+			BehaviourReturnCode onInterruptReturn)
+		{
+			_behaviourComponent = behaviour;
+			_interruptCondition = interruptCondition;
+			_onInterruptReturn = onInterruptReturn;
+		}
+
+		public override BehaviourReturnCode Behave()
+		{
+			try
+			{
+				if (_interruptCondition.Behave() == BehaviourReturnCode.Success)
+				{
+					return _onInterruptReturn;
+				}
+
+				return _behaviourComponent.Behave();
+			}
+			catch (Exception e)
+			{
+#if DEBUG
+				Console.Error.WriteLine(e.ToString());
+#endif
+				ReturnCode = BehaviourReturnCode.Failure;
+				return ReturnCode;
+			}
+		}
+	}
+}
