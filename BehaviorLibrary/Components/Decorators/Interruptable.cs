376f670a82ef19c371e8dc107ec73dbf336fc1f1
Michael O'Kane <ml_o_kane@hotmail.com>
2013-10-10 16:53:06 +0100

    Added comments

--- BehaviorLibrary/Components/Decorators/Interruptable.cs
+++ BehaviorLibrary/Components/Decorators/Interruptable.cs
@@ -9,6 +9,18 @@ namespace BehaviourLibrary.Components.Decorators
 		private readonly Conditional _interruptCondition;
 		private readonly BehaviourReturnCode _onInterruptReturn;
 
+		/// <summary>
+		/// Checks an interrupt condition, executing the given behaviour only if the condition returns False
+		/// -Returns Success if the given behaviour returns Success and the condition returns False
+		/// -Returns Running if the given behaviour returns Running and the condition returns False
+		/// -Returns Failure if the given behaviour returns Failure or the condition returns True
+		/// 
+		/// Possibly not a good solution for interrupt style behaviour in the long run as it is very difficult to write
+		/// conditions for interrupting without adding lots of state elsewhere to track when interrupts occur
+		/// </summary>
+		/// <param name="behaviour"></param>
+		/// <param name="interruptCondition"></param>
+		/// <param name="onInterruptReturn"></param>
 		public Interruptable(BehaviourComponent behaviour, Conditional interruptCondition,
 			BehaviourReturnCode onInterruptReturn)
 		{
